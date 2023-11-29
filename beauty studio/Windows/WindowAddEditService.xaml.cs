dd8a7b2ca34fc123bfad5f6a3d011ae8e74a0e95
Margarita <rita.fedyukevitch@yandex.ru>
2023-11-29 12:31:54 +0500

    Добавьте файлы проекта.

---
+++ beauty studio/Windows/WindowAddEditService.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace beauty_studio.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowAddEditService.xaml
+    /// </summary>
+    public partial class WindowAddEditService : Window
+    {
+        public WindowAddEditService(Service service)
+        {
+            InitializeComponent();
+            service.Title = tbName.Text;
+            service.Description = tbDescription.Text;
+            service.Cost = int.Parse(tbCost.Text);
+            service.DurationInSeconds = int.Parse(tbDuration.Text);
+            service.Discount = tbDiscount.Text;
+            service.MainImagePath = tbManeImage.Text;
+        }
+        Service service;
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            tbName.Text = service.Title; 
+            tbDescription.Text = service.Description;
+            tbManeImage.Text = service.MainImagePath;
+            tbDuration.Text = service.DurationInSeconds.ToString();
+            tbCost.Text = service.Cost.ToString();
+            tbDiscount.Text = service.Discount.ToString();
+            if (service.ID == 0)
+                App.beauty_Studio.SaveChanges();
+            Close();
+        }
+
+        private void btnCansel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ beauty studio/Windows/WindowService.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace beauty_studio.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowService.xaml
+    /// </summary>
+    public partial class WindowService : Window
+    {
+        public WindowService()
+        {
+            InitializeComponent();
+            dgService.ItemsSource = App.beauty_Studio.Service.ToList();
+        }
+        private void btnAddService_Click(object sender, RoutedEventArgs e)
+        {
+            WindowAddEditService windowAdd = new WindowAddEditService();
+            windowAdd.ShowDialog();
+            dgService.ItemsSource = null;
+            dgService.ItemsSource = App.beauty_Studio.Service.ToList();
+        }
+
+        private void btnEditService_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            { 
+                Service service = dgService.SelectedItem as Service;
+                WindowAddEditService editService = new WindowAddEditService(service);
+                editService.ShowDialog();
+                dgService.ItemsSource = null;
+                dgService.ItemsSource = App.beauty_Studio.Service.ToList();
+            }catch { MessageBox.Show("Выберите запись для редактирования"); }
+        }
+
+        private void btnDeleteService_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Service service = dgService.SelectedItem as Service;
+                App.beauty_Studio.Service.Remove(service);
+                App.beauty_Studio.SaveChanges();
+                dgService.ItemsSource = null;
+                dgService.ItemsSource = App.beauty_Studio.Service.ToList();
+            }
+            catch { MessageBox.Show("Выберите запись для удаления"); }
+        }
+    }
+}
